f3bb8ba8fe2535c4466a08d09c2e1d760725c8fa
YomiHub <1348918467@qq.com>
2021-10-28 19:11:40 +0800

    循环队列

--- 2_Queue.js
+++ 2_Queue.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Yomi
  * @Date: 2021-09-12 10:16:58
- * @LastEditors: Yomi
- * @LastEditTime: 2021-09-12 10:21:40
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-10-27 09:42:57
  */
 //优先级队列--------------------------------------------
 function Queue() {
@@ -58,3 +58,82 @@ function playGame(arr, baseNum) {
 var names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl'];
 var index = playGame(names, 7) // 数到8的人淘汰
 console.log(index);
+
+
+//实现循环队列
+var MyCircularQueue = function(k) {
+  this.items = new Array(k+1);
+  this.front = 0;
+  this.tail = 0; 
+};
+
+/** 向循环队列插入一个元素。如果成功插入则返回真
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.enQueue = function(value) {
+  if(this.isFull()){
+    return false;
+  }else{
+    this.items[this.tail] = value;
+    this.tail = (this.tail+1)%this.items.length;
+    return true;
+  }
+};
+
+/**从循环队列中删除一个元素。如果成功删除则返回真
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.deQueue = function() {
+  if(this.isEmpty()){
+    return false;
+  }else{
+    this.front = (this.front+1)%this.items.length;
+    return true;
+  }
+};
+
+/** 从队首获取元素。如果队列为空，返回 -1 
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function() {
+  if(this.isEmpty()){
+    return -1;
+  }else{
+    return this.items[this.front];
+  }
+};
+
+/**获取队尾元素。如果队列为空，返回 -1 
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function() {
+  if(this.isEmpty()){
+    return -1;
+  }else{
+    return this.items[(this.tail-1+this.items.length)%this.items.length]
+  }
+};
+
+/**检查循环队列是否为空
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function() {
+  if(this.front == this.tail){
+    return true;
+  }else{
+    return false;
+  }
+};
+
+/**检查循环队列是否已满
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function() {
+  if((this.tail+1)%this.items.length==this.front){
+    return true;
+  }else{
+    return false;
+  }
+};
+
