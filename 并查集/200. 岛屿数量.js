6906dcb08c5ff8cb5770f0732bb070fb5d39def3
YomiHub <1348918467@qq.com>
2021-12-01 15:35:52 +0800

    岛屿数量（并查集）

--- 并查集/200. 岛屿数量.js
+++ 并查集/200. 岛屿数量.js
@@ -47,10 +47,120 @@ var numIslands = function(grid) {
 };
 
 
-//BFS
-var numIslands = function(grid) {
+// BFS (染色思路) 将遍历到1的位置，和它相邻的1都染成0
+var numIslands = function (grid) {
+  if (grid[0].length === 0) return 0
 
+  let res = 0
+  let rowLen = grid.length
+  let colLen = grid[0].length
+  let directions = [[-1,0],[1,0],[0,-1],[0,1]];
+
+  for (let i = 0; i < rowLen; ++i) {
+    for (let j = 0; j < colLen; ++j) {
+      if (grid[i][j]==1) {
+        //将1和相邻的1都变成0
+        let queues = [[i,j]];
+        res++;
+        while(queues.length){
+          let [x1,y1] = queues.shift(); //取出1
+          if(grid[x1][y1]!=1){continue;};
+          grid[x1][y1] = 0;
+
+          for(let [dx,dy] of directions){
+            let x2 = x1+dx, y2 = y1+dy;
+            if(x2>=0&&x2<rowLen&&y2>=0&&y2<colLen){
+              queues.push([x2,y2]);
+            }
+          }
+        }
+      }
+    }
+  }
+  return res;
 }
+
+//并查集：初始化时将结点的root指向自己
+class UnionFind{
+  constructor(len){
+    this.count = len; //并查集总数
+    this.parent = new Array(len); //用于存root
+    this.rank = new Array(len); //用于优化并查集深度
+
+    for(let i = 0; i<len; ++i){
+      this.parent[i] = i; //初始化时各自为营
+      this.rank[i] = 1; //存储每个集合包含的结点数，表示深度
+    }
+  }
+
+  //查找结点的root
+  findRoot(x){
+    //root是自己指向自己的结点  this.parent[-1] == undefined
+    while(this.parent[x]!=x){
+      // 进行路径压缩
+      this.parent[x] = this.parent[this.parent[x]];
+      x = this.parent[x];
+    }
+    return x;
+  }
+
+  //将p和q加入到一个集合（连通）
+  union(p,q){
+    //先找到p、q的root
+    let pRoot = this.findRoot(p);  //p为-1，得到的是undefined
+    let qRoot = this.findRoot(q);
+    if(pRoot===qRoot) return;
+
+    if(this.rank[pRoot]>this.rank[qRoot]){
+      //将深度小的连接到深度大的p中
+      this.parent[qRoot] = pRoot;
+      this.rank[pRoot] += this.rank[qRoot];
+    }else {
+      //两者相等时rank++
+      this.parent[pRoot] = qRoot;
+      this.rank[qRoot] += this.rank[pRoot];
+    }
+    this.count--; //连通后集合数减少
+  }
+
+  //返回集合数
+  getCount(){
+    return this.count;
+  }
+}
+
+var numIslands = function (grid) {
+  if (grid[0].length === 0) return 0
+
+  let rowLen = grid.length
+  let colLen = grid[0].length
+  let directions = [[1, 0], [0, 1]]  //因为是顺序遍历，向右向下扫描即可
+  let dummy = -1; //用于union矩阵中的0，count--
+  let unionSet = new UnionFind(rowLen*colLen); //创建并查集
+
+  for (let i = 0; i < rowLen; ++i) {
+    for (let j = 0; j < colLen; ++j) {
+      if(grid[i][j]==='0'){
+        unionSet.union(i*colLen+j,dummy);
+      }else if(grid[i][j]==='1'){
+        for(let [dx,dy] of directions){
+          let x = i+dx, y = j+dy;
+          //下标合法
+          if(x>=rowLen||y>colLen) continue;
+          if(grid[x][y]==='1'){
+            unionSet.union(i*colLen+j,x*colLen+y);//合并
+          }
+        }
+      }
+    }
+  }
+  return unionSet.getCount();
+}
+
+
+let test = [["1","1","1"],["1","0","1"],["1","1","1"]];
+console.log(numIslands(test))
+
 
 let test = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]];
 console.log(numIslands(test))
